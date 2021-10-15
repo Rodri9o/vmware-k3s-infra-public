dd989612cd3009ef4dedcf854bdab093bed5a187
samgabrail <sameh.gabrail@gmail.com>
2021-10-15 18:49:02 -0400

    fix module output

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
-output "master_ips" {
-  value = module.vsphere_vm_master.ips
-}
+# output "master_ips" {
+#   value = {
+#     for vm in module.vsphere_vm_master.ip :
+#     vm.name => vm.guest_ip_addresses[0]
+#   }
+# }
 
-output "worker_ips" {
-  value = module.vsphere_vm_worker.ips
-}
+# # {
+# #       for vm in vsphere_virtual_machine.vm:
+# #         vm.name => vm.guest_ip_addresses[0]
+# #   }
+
+# output "worker_ips" {
+#   value = module.vsphere_vm_worker.ips
+# }
