cd9709c3975895f677d8cf9cd401429dd576c4ce
samgabrail <sameh.gabrail@gmail.com>
2021-10-15 17:38:22 -0400

    destroy

--- ip-static.sh
+++ ip-static.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 # this is a script to force static IPs for the VM as customization still uses the old /etc/network/interfaces to populate the static ip. So if we don't run this, the VM will come up with a DHCP IP.
-cat > /etc/netplan/10-netcfg.yaml <<-EOF
-network:
-  version: 2
-  ethernets:
-    ens192:
-      addresses: [$1/24]
-      gateway4: $2
-      nameservers:
-        addresses: [$3, $4]
-EOF
-rm /etc/netplan/00-installer-config.yaml
-netplan apply
-exit 0
+# Below is no longer used since we now use Cloud-init with TF
+
+# cat > /etc/netplan/10-netcfg.yaml <<-EOF
+# network:
+#   version: 2
+#   ethernets:
+#     ens192:
+#       addresses: [$1/24]
+#       gateway4: $2
+#       nameservers:
+#         addresses: [$3, $4]
+# EOF
+# rm /etc/netplan/00-installer-config.yaml
+# netplan apply
+# exit 0
