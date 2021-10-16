302cca6634f589b734123b9b5486b7866220d950
samgabrail <sameh.gabrail@gmail.com>
2021-10-16 08:54:14 -0400

    remove output for now

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
-output "master_ips" {
-  value = {
-    for vm in module.vsphere_vm_master :
-    vm.ip => vm.guest_ip_addresses[0]
-  }
-}
-
-# # {
-# #       for vm in vsphere_virtual_machine.vm:
-# #         vm.name => vm.guest_ip_addresses[0]
-# #   }
-
-# output "worker_ips" {
-#   value = module.vsphere_vm_worker.ips
+# output "master_ips" {
+#   value = {
+#     for vm in module.vsphere_vm_master :
+#     vm.ip => vm.guest_ip_addresses[0]
+#   }
 # }
 
-output "worker_ips" {
-  value = {
-    for vm in module.vsphere_vm_worker :
-    vm.ip => vm.guest_ip_addresses[0]
-  }
-}
+# # # {
+# # #       for vm in vsphere_virtual_machine.vm:
+# # #         vm.name => vm.guest_ip_addresses[0]
+# # #   }
+
+# # output "worker_ips" {
+# #   value = module.vsphere_vm_worker.ips
+# # }
+
+# output "worker_ips" {
+#   value = {
+#     for vm in module.vsphere_vm_worker :
+#     vm.ip => vm.guest_ip_addresses[0]
+#   }
+# }
