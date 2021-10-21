114e31eabd2b4e8786e6d4a0cc82bf382eaf08eb
samgabrail <sameh.gabrail@gmail.com>
2021-10-21 15:03:59 -0400

    build k3s

---
+++ installk3s.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+arrMASTER_NAMES=($1)
+arrWORKER_NAMES=($2)
+arrMASTER_IPS=($3)
+arrWORKER_IPS=($4)
+USER_K3S=$5
+K3S_VERSION=$6
+K3S_CONTEXT=$7
+
+echo $arrMASTER_NAMES
+echo $arrWORKER_NAMES
+echo $arrMASTER_IPS
+echo $arrWORKER_IPS
+echo $USER_K3S
+echo $K3S_VERSION
+echo $K3S_CONTEXT
+
+curl -sLS https://get.k3sup.dev | sh
+sudo install k3sup /usr/local/bin/
+# I'm only installing one master node, I haven't looked into multi-master nodes for HA
+k3sup install --ip ${arrMASTER_IPS[0]} --local-path ~/.kube/config --k3s-version ${K3S_VERSION} --context ${K3S_CONTEXT} --user ${USER_K3S}
+sleep 20
+
+for (( i=0; i<${#arrWORKER_IPS[@]}; i++ ))
+    do  
+        k3sup join --ip ${arrWORKER_IPS[i]} --server-ip ${arrMASTER_IPS[0]} --user ${USER_K3S} --k3s-version ${K3S_VERSION} || true
+    done
+
+sleep 20
+kubectl get nodes -owide
+kubectl taint nodes ${arrMASTER_NAMES[0]} key=value:NoSchedule || true
